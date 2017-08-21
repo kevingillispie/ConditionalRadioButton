5d3f7803a49b1f2043d7a7ad8c727ff234103658
Kevin_Gillispie <oakenquill@gmail.com>
2017-08-21 16:26:08 -0700

    Add project files.

---
+++ ConditionalRadioButton/NoteTakingPref.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NoteTakingPref.aspx.cs" Inherits="ConditionalRadioButton.NoteTakingPref" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Note Taking Preferences</title>
+</head>
+<body>
+    <p>Your Note Takeing Preferences</p>
+    <form id="form1" runat="server">
+        <div>
+            <asp:RadioButton ID="RadioButton1" runat="server" GroupName="Supplies" Text="Pencil" />
+            <br />
+            <asp:RadioButton ID="RadioButton2" runat="server" GroupName="Supplies" Text="Pen" />
+            <br />
+            <asp:RadioButton ID="RadioButton3" runat="server" GroupName="Supplies" Text="Phone" />
+            <br />
+            <asp:RadioButton ID="RadioButton4" runat="server" GroupName="Supplies" Text="Tablet" />
+            <br />
+            <br />
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="OK" />
+            <br />
+            <br />
+            <asp:Image ID="resultImage" runat="server" Height="200px" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ConditionalRadioButton/NoteTakingPref.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConditionalRadioButton
+{
+    public partial class NoteTakingPref : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            if (RadioButton1.Checked)
+            {
+                resultLabel.Text = "You selected pencil.";
+                resultImage.ImageUrl = "images/pencil.jpg";
+            }
+            else if (RadioButton2.Checked)
+            {
+                resultLabel.Text = "You selected pen.";
+                resultImage.ImageUrl = "images/pen.png";
+            }
+            else if (RadioButton3.Checked)
+            {
+                resultLabel.Text = "You selected phone.";
+                resultImage.ImageUrl = "images/phone.jpg";
+            }
+            else if (RadioButton4.Checked)
+            {
+                resultLabel.Text = "You selected tablet.";
+                resultImage.ImageUrl = "images/tablet.jpg";
+            }
+            else
+            {
+                resultLabel.Text = "Please choose one.";
+                resultImage.ImageUrl = "images/arrow.png";
+            }
+        }
+    }
+}
